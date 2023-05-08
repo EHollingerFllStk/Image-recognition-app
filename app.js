1e8d9d5f96734a8441405bfb47ad2a4b5733dce2
Beth-Hollinger <EHollingerFullStackEngineer@gmail.com>
2023-05-08 17:04:20 -0400

    complete routes and tested with postman

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const http = require('http');
+const predictRouter = require('./routes/predict');
 
 const app = express();
 //Add body-parser to access 'body' in 'req' later
@@ -12,7 +13,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function (req, res) {
     res.send("hello")
 })
-
+app.use('/predict', predictRouter);
 ////Listener//////
 const port = process.env.PORT || '8080';
 
