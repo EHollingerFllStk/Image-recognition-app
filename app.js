c00d1fdc0de6c28cca5e2b9bca48c517c865c540
Beth-Hollinger <EHollingerFullStackEngineer@gmail.com>
2023-05-08 14:09:12 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+/////Dependencies/////
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const http = require('http');
+
+const app = express();
+//Add body-parser to access 'body' in 'req' later
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', function (req, res) {
+    res.send("hello")
+})
+
+////Listener//////
+const port = process.env.PORT || '8080';
+
+app.set('port', port);
+const server = http.createServer(app);
+server.listen(port)
+server.on('listening', () => {
+    console.log('Listening on ' + (port));
+});
